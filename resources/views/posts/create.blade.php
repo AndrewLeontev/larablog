ee4ec209de45736fa0cf8f7e30634a9aaf8352c4
Andrey Leontev <leontev.andrey.s@gmail.com>
2018-12-24 08:00:52 +1000

    Fix upload button for post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -33,12 +33,9 @@
               <input type="text" class="form-control" id="tags"  name="tags">
           </div>
 
-          <div class="form-group">
-            <label for="post_image">Post image</label>
-            {{-- <input type="file" class="form-control" id="post_image"  name="post_image"> --}}
-          <input type="file" id="post_image" name="post_image">
 
-          </div>
+          <input type="file" name="post_image" id="post_image" class="inputfile inputfile-2">
+          <label for="post_image"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path></svg> <span>Choose a file…</span></label>
 
           <center><button type="submit" class="btn btn-primary">Post</button></center>
         </form>
